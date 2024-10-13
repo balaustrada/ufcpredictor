f9da75f1805d00ca1a8a546bd6ae5ecdadbc7d2b
César Ramírez-Pérez <cramirpe@gmail.com>
2024-10-13 23:38:35 +0200

    first app version

---
+++ app.py
@@ -0,0 +1,133 @@
+import gradio as gr
+import numpy as np
+import logging
+import torch
+from datetime import datetime
+import sys
+
+from ufcpredictor.utils import convert_odds_to_decimal, convert_odds_to_moneyline
+from ufcpredictor.datasets import ForecastDataset
+from ufcpredictor.data_processor import WOSRDataProcessor as DataProcessor
+from ufcpredictor.models import SymmetricFightNet
+from ufcpredictor.loss_functions import BettingLoss
+from ufcpredictor.utils import convert_odds_to_decimal
+
+
+logger = logging.getLogger(__name__)
+
+X_set = [
+    "clinch_strikes_att_opponent_per_minute",
+    "time_since_last_fight",
+    "total_strikes_succ_opponent_per_minute",
+    "takedown_succ_per_minute",
+    "KO_opponent_per_minute",
+    "takedown_att_per_minute",
+    "takedown_succ_opponent_per_minute",
+    "win_opponent_per_fight",
+    "head_strikes_succ_opponent_per_minute",
+    "clinch_strikes_succ_opponent_per_minute",
+    "ground_strikes_succ_opponent_per_minute",
+    "ground_strikes_att_per_minute",
+    "head_strikes_succ_per_minute",
+    "age",
+    "distance_strikes_succ_per_minute",
+    "body_strikes_succ_per_minute",
+    "strikes_succ_opponent_per_minute",
+    "leg_strikes_att_per_minute",
+    "reversals_opponent_per_minute",
+    "strikes_succ_per_minute",
+    "distance_strikes_att_opponent_per_minute",
+    "Sub_opponent_per_fight",
+    "distance_strikes_att_per_minute",
+    "knockdowns_per_minute",
+    "OSR",
+]
+
+
+def predict(a, b, c, d):
+    return a, b, c, d
+
+
+def greet(name):
+    return "Hello " + name + "!"
+
+def main():
+    logger.info("Loading data...")
+    data_processor = DataProcessor("/home/cramirez/kaggle/ufc_scraper/data")
+    data_processor.load_data()
+    data_processor.aggregate_data()
+    data_processor.add_per_minute_and_fight_stats()
+    data_processor.normalize_data()
+
+    logger.info("Creating dataset from loaded data...")
+    dataset = ForecastDataset(
+        data_processor=data_processor,
+        X_set=X_set,
+    )
+
+    logger.info("Loading model...")
+    model = SymmetricFightNet(
+        input_size=len(X_set),
+        dropout_prob=0.35,
+    )
+    model.load_state_dict(
+        torch.load("/home/cramirez/kaggle/ufc_scraper/ufcpredictor/notebooks/model.pth")
+    )
+
+    fighter_names = list(
+        data_processor.scraper.fighter_scraper.data["fighter_name"].values
+    )
+
+    with gr.Blocks() as demo:
+        event_date = gr.DateTime(label="Event Date", include_time=False, value=datetime.now().strftime("%Y-%m-%d"))
+        fighter_name = gr.Dropdown(
+            label="Fighter Name",
+            choices=fighter_names,
+            interactive=True,
+        )
+        opponent_name = gr.Dropdown(
+            label="Opponent Name",
+            choices=fighter_names,
+            interactive=True,
+        )
+        odds1 = gr.Number(label="Odds 1", value=100)
+        odds2 = gr.Number(label="Odds 2", value=100)
+
+        output = gr.Text(label="Prediction Output")
+
+        btn = gr.Button("Predict")
+
+        def get_forecast_single_prediction(fighter_name, opponent_name, event_date, odds1, odds2):
+            event_date = [datetime.fromtimestamp(event_date).strftime("%Y-%m-%d"),]
+            odds1 = convert_odds_to_decimal([odds1,])
+            odds2 = convert_odds_to_decimal([odds2,])
+
+            #print(fighter_name, opponent_name, event_date, odds1, odds2)
+            p1, p2 =  dataset.get_forecast_prediction(
+                [fighter_name,],
+                [opponent_name,],
+                event_date,
+                odds1,
+                odds2,
+                model=model
+            )
+            prediction = (p1 + p2) / 2
+            shift = np.abs(p1 - p2)
+
+            return f"{prediction[0][0]:.3f} ({shift[0][0]:.3f})"
+        
+        btn.click(
+            get_forecast_single_prediction,
+            inputs=[fighter_name, opponent_name, event_date, odds1, odds2], 
+            outputs=output
+        )
+
+    demo.launch()
+
+if __name__ == "__main__":
+    # logging.basicConfig(
+    #     stream=sys.stdout,
+    #     level="INFO",
+    #     format="%(levelname)s:%(message)s",
+    # )
+    main()
--- notebooks/modular_predictor_var1.1_OSR_2_train_phases.py
+++ notebooks/modular_predictor_var1.1_OSR_2_train_phases.py
@@ -82,15 +82,31 @@ invalid_fights = set(self.data_aggregated[self.data_aggregated["num_fight"] < 4]
 # invalid_fights |= set(self.data_aggregated[self.data_aggregated["event_date"] < "2013-01-01"]["fight_id"])
 
 # %%
-split_date = "2024-09-01"#"2023-08-01"
+early_split_date = "2018-01-01"
+split_date = "2023-08-01"#"2023-08-01"
 
-train_fights = self.data["fight_id"][self.data["event_date"] < split_date]
+early_train_fights = self.data["fight_id"][self.data["event_date"] < split_date]
+
+train_fights = self.data["fight_id"][(
+        (self.data["event_date"] < split_date)
+        & (self.data["event_date"] >= early_split_date)
+    )
+]
+
+    
 test_fights  = self.data["fight_id"][self.data["event_date"] >= split_date]
 
+early_train_fights = set(early_train_fights) - set(invalid_fights)
 train_fights = set(train_fights) - set(invalid_fights)
 test_fights = set(test_fights) - set(invalid_fights)
 
 # %%
+early_train_dataset = BasicDataset(
+    data_processor,
+    early_train_fights,
+    X_set=X_set,
+)
+
 train_dataset = BasicDataset(
     data_processor,
     train_fights,
@@ -104,6 +120,7 @@ test_dataset = BasicDataset(
 )
 
 # %%
+early_train_dataloader = torch.utils.data.DataLoader(early_train_dataset, batch_size=64, shuffle=True)
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
 
@@ -139,7 +156,14 @@ trainer = Trainer(
 )   
 
 # %%
-trainer.train(epochs=8) # ~8 is a good match if dropout to 0.35 
+trainer.train(
+    epochs=5,
+    train_loader=early_train_dataloader,
+    test_loader=test_dataloader,
+)
+
+# %%
+trainer.train(epochs=3) # ~8 is a good match if dropout to 0.35 
 
 # %%
 # Save model dict

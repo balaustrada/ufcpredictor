2130627836161ea5c5adeebd062ec8fe49b8be7a
César Ramírez-Pérez <cramirpe@gmail.com>
2024-10-14 12:26:40 +0200

    format app

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def predict(a, b, c, d):
 def greet(name):
     return "Hello " + name + "!"
 
+
 def main(args: Optional[argparse.Namespace] = None) -> None:
     if args is None:
         args = get_args()
@@ -87,16 +88,18 @@ def main(args: Optional[argparse.Namespace] = None) -> None:
         input_size=len(X_set),
         dropout_prob=0.35,
     )
-    model.load_state_dict(
-        torch.load(args.model_path)
-    )
+    model.load_state_dict(torch.load(args.model_path))
 
     fighter_names = list(
         data_processor.scraper.fighter_scraper.data["fighter_name"].values
     )
 
     with gr.Blocks() as demo:
-        event_date = gr.DateTime(label="Event Date", include_time=False, value=datetime.now().strftime("%Y-%m-%d"))
+        event_date = gr.DateTime(
+            label="Event Date",
+            include_time=False,
+            value=datetime.now().strftime("%Y-%m-%d"),
+        )
         fighter_name = gr.Dropdown(
             label="Fighter Name",
             choices=fighter_names,
@@ -107,34 +110,49 @@ def main(args: Optional[argparse.Namespace] = None) -> None:
             choices=fighter_names,
             interactive=True,
         )
-        odds1 = gr.Number(label="Odds 1", value=100)
-        odds2 = gr.Number(label="Odds 2", value=100)
+        odds1 = gr.Number(label="Fighter odds", value=100)
+        odds2 = gr.Number(label="Opponent odds", value=100)
 
         btn = gr.Button("Predict")
 
         output = gr.Plot(label="")
         # output = gr.Text(label="Prediction Output")
 
+        def get_forecast_single_prediction(
+            fighter_name, opponent_name, event_date, odds1, odds2
+        ):
+            event_date = [
+                datetime.fromtimestamp(event_date).strftime("%Y-%m-%d"),
+            ]
+            odds1 = convert_odds_to_decimal(
+                [
+                    odds1,
+                ]
+            )
+            odds2 = convert_odds_to_decimal(
+                [
+                    odds2,
+                ]
+            )
 
-        def get_forecast_single_prediction(fighter_name, opponent_name, event_date, odds1, odds2):
-            event_date = [datetime.fromtimestamp(event_date).strftime("%Y-%m-%d"),]
-            odds1 = convert_odds_to_decimal([odds1,])
-            odds2 = convert_odds_to_decimal([odds2,])
-
-            #print(fighter_name, opponent_name, event_date, odds1, odds2)
-            p1, p2 =  dataset.get_forecast_prediction(
-                [fighter_name,],
-                [opponent_name,],
+            # print(fighter_name, opponent_name, event_date, odds1, odds2)
+            p1, p2 = dataset.get_forecast_prediction(
+                [
+                    fighter_name,
+                ],
+                [
+                    opponent_name,
+                ],
                 event_date,
                 odds1,
                 odds2,
-                model=model
+                model=model,
             )
-            prediction = (p1 + p2)[0][0] - 1 # Prediction between -1 and 1
+            prediction = (p1 + p2)[0][0] - 1  # Prediction between -1 and 1
             shift = np.abs(p1 - p2)[0][0] * 2
 
-            prediction *=100
-            shift*=100
+            prediction *= 100
+            shift *= 100
 
             fig, ax = plt.subplots(figsize=(6.4, 1.7))
             red = "tab:red"
@@ -163,50 +181,52 @@ def main(args: Optional[argparse.Namespace] = None) -> None:
 
             ax.text(
                 ax.get_xlim()[0],
-                ax.get_ylim()[1]*1.3,
+                ax.get_ylim()[1] * 1.3,
                 fighter_name,
                 color=red,
                 ha="left",
                 va="center",
                 fontsize=12,
-                fontweight='bold',
+                fontweight="bold",
             )
 
             ax.text(
                 ax.get_xlim()[1],
-                ax.get_ylim()[1]*1.3,
+                ax.get_ylim()[1] * 1.3,
                 opponent_name,
                 color=blue,
                 ha="right",
                 va="center",
                 fontsize=12,
-                fontweight='bold',
+                fontweight="bold",
             )
 
-            ax.axvline(x=0, color='lightgray', lw=1)
+            ax.axvline(x=0, color="lightgray", lw=1)
             ax.text(
-                prediction*1.2,
-                ax.get_ylim()[1]*0.5,
+                prediction * 1.2,
+                ax.get_ylim()[1] * 0.5,
                 f"{abs(prediction):.2f}±{shift:.2f}",
                 color=color,
-                ha='left' if prediction > 0 else "right",
+                ha="left" if prediction > 0 else "right",
                 va="center",
                 fontsize=11,
-                fontweight='bold',
+                fontweight="bold",
             )
-            fig.subplots_adjust(left=0.1, right=0.9, top=0.75, bottom=0.2)  # Adjust margins as needed
+            fig.subplots_adjust(
+                left=0.1, right=0.9, top=0.75, bottom=0.2
+            )  # Adjust margins as needed
 
-    
             return fig
-            #return f"{prediction[0][0]:.3f} ({shift[0][0]:.3f})"
-        
+            # return f"{prediction[0][0]:.3f} ({shift[0][0]:.3f})"
+
         btn.click(
             get_forecast_single_prediction,
-            inputs=[fighter_name, opponent_name, event_date, odds1, odds2], 
-            outputs=output
+            inputs=[fighter_name, opponent_name, event_date, odds1, odds2],
+            outputs=output,
         )
 
     demo.launch(server_name=args.server_name, server_port=args.port)
+
 
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -240,6 +260,7 @@ def get_args() -> argparse.Namespace:
     )
 
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     main()

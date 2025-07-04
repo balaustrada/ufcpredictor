9b23a56143e8e149885763d48060a52c6bf0df65
César Ramírez-Pérez <cramirpe@gmail.com>
2025-07-04 20:20:08 +0200

    fix(utils): Return np arrays

--- ufcpredictor/utils_sheets.py
+++ ufcpredictor/utils_sheets.py
@@ -1,12 +1,13 @@
 """
-This module provides functionality to read data from Google Sheets using the Google 
+This module provides functionality to read data from Google Sheets using the Google
 Sheets API.
-It includes a class `SheetsReader` for handling authentication and reading data, and a 
+It includes a class `SheetsReader` for handling authentication and reading data, and a
 function `read_fights_sheet` to read specific fight data from a given spreadsheet.
 """
 
 from __future__ import annotations
 
+import numpy as np
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -18,6 +19,8 @@ from googleapiclient.errors import HttpError
 
 if TYPE_CHECKING:  # pragma: no cover
     from typing import Callable, Dict, Optional
+
+    from numpy.typing import NDArray
 
 
 class SheetsReader:
@@ -47,7 +50,7 @@ class SheetsReader:
     def authenticate(self) -> None:
         """Authenticates the user and initializes the service."""
         try:
-            self.creds = Credentials.from_authorized_user_file( # type: ignore[no-untyped-call]
+            self.creds = Credentials.from_authorized_user_file(  # type: ignore[no-untyped-call]
                 self.creds_file, self.scopes
             )
         except Exception as e:
@@ -59,7 +62,7 @@ class SheetsReader:
 
         if not self.creds or not self.creds.valid:
             if self.creds and self.creds.expired and self.creds.refresh_token:
-                self.creds.refresh(Request()) # type: ignore[no-untyped-call]
+                self.creds.refresh(Request())  # type: ignore[no-untyped-call]
             else:
                 raise Exception("Invalid credentials")
 
@@ -87,19 +90,18 @@ def read_fights_sheet(
     spreadsheet_id: str,
     creds_file: Path,
     fields_to_read: list[str],
-    transformations_to_apply: Optional[list[Callable]] = None,
-) -> Dict[str, list]:
+    dtypes_: Optional[List[type]] = None,
+) -> list[NDArray]:
     """Reads the fights sheet from the specified Google Sheets document.
 
     Args:
         spreadsheet_id: The ID of the Google Sheets document.
         creds_file: The path to the credentials file.
         fields_to_read: The list of fields (columns) to read from the sheet.
-        transformations_to_apply: A list of transformation functions to apply to each 
-            column.
+        dtypes_: List with the numpy types to apply to each column.
 
     Returns:
-        Dict[str, list]: A dictionary containing the requested fields and their values.
+        List of numpy arrays with the column informations.
     """
     reader = SheetsReader(
         creds_file=creds_file,
@@ -122,24 +124,16 @@ def read_fights_sheet(
     if not all(len(col) == len(columns[0]) for col in columns):
         raise ValueError("All columns must have the same length.")
 
-    data_dict = {}
+    output_arrays = []
     for i, col_name in enumerate(fields_to_read):
         if col_name in headers:
             index = headers.index(col_name)
-            data_dict[col_name] = columns[index]
+            output_arrays.append(np.asarray(columns[index]))
         else:
             raise ValueError(f"Column '{col_name}' not found in the sheet.")
 
-    if transformations_to_apply:
-        # check length of transformations_to_apply matches length of columns_to_read
-        if len(transformations_to_apply) != len(fields_to_read):
-            raise ValueError(
-                "Number of transformations does not match number of columns to read."
-            )
+    if dtypes_:
+        for i, dtype_ in enumerate(dtypes_):
+            output_arrays[i] = output_arrays[i].astype(dtype_)
 
-        for i, (header, column) in enumerate(data_dict.items()):
-            data_dict[header] = [
-                transformations_to_apply[i](val) if val else None for val in column
-            ]
-
-    return data_dict
+    return output_arrays
